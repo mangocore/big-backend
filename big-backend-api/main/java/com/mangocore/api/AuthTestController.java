a7a7b5b6be1360d2c3ed79de8a610fab1e4de9c4
notreami <notreami@163.com>
2017-11-16 17:01:17 +0800

    增加feign

--- big-backend-api/main/java/com/mangocore/api/AuthTestController.java
+++ big-backend-api/main/java/com/mangocore/api/AuthTestController.java
@@ -1,6 +1,10 @@
 package com.mangocore.api;
 
+import com.mangocore.common.response.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/api/test")
+@PreAuthorize("hasAuthority('AUTH_WRITE')")
 public class AuthTestController {
+
+    @PostMapping("/login")
+    public CommonResponse login() {
+        return CommonResponse.createSuccess(null);
+    }
+
+    @GetMapping("/hello")
+    public CommonResponse hello() {
+        return CommonResponse.createSuccess(null);
+    }
+
+    @GetMapping("/world")
+    @PreAuthorize("hasRole('ADMIN')")
+    public CommonResponse world() {
+        return CommonResponse.createSuccess(null);
+    }
+
 }
