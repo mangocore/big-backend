b2279b05d87ee71e02b4920dd853f2f21483f472
notreami <notreami@163.com>
2017-11-17 16:54:06 +0800

    删除多模块

--- big-backend-server/docker/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-cd ./../..
-pwd
-./gradlew clean --refresh-dependencies -x test -x check assemble -P projectEnv=prod --stacktrace
-
-mv -f ./big-backend-server/build/libs/big-backend.jar ./big-backend-server/docker
